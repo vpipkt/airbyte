cd39094f4d5748d2a30811270aa7f54b8e0eb580
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-01-31 10:58:08 +0100

    bust cache for sha

--- airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/format/AvroFormat.java
+++ airbyte-integrations/connectors/source-kafka/src/main/java/io/airbyte/integrations/source/kafka/format/AvroFormat.java
@@ -61,6 +61,7 @@ public class AvroFormat extends AbstractFormat {
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
     props.put(SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+    // these are not really optional, as written
 //    props.put(SchemaRegistryClientConfig.USER_INFO_CONFIG,
 //        String.format("%s:%s", avro_config.get("schema_registry_username").asText(), avro_config.get("schema_registry_password").asText()));
     props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, avro_config.get("schema_registry_url").asText());

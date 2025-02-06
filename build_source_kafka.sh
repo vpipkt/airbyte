b12a4fe5665a3b55173e70c13929520b10a69c4b
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-02-06 12:18:42 +0100

    build tweak

--- build_source_kafka.sh
+++ build_source_kafka.sh
@@ -5,7 +5,11 @@ TAG="dev-${ARCH}-${SHA}"
 
 echo $TAG;
 
+# and then this flag in the airbyte-ci command / build subcommand --use-host-gradle-dist-tar && \
+
 ./gradlew :airbyte-integrations:connectors:source-kafka:build && \
   airbyte-ci connectors --name=source-kafka build -a "linux/${ARCH}" --tag "${TAG}" --use-host-gradle-dist-tar && \
   docker tag airbyte/source-kafka:${TAG} vpipkt/source-kafka:${TAG} && \
   docker push vpipkt/source-kafka:${TAG}
+
+echo "done"
